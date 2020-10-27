4b131a2748ac5f07f284627af422aafbc5af9240
Steven Mathey <72931382+sethyvenatem@users.noreply.github.com>
2020-10-27 13:26:48 +0100

    Update pe_46.py
    I removed one call of P in the outer loop and the primality check on n in the inner loop. Now it takes 256 seconds (before it was 522).
    
    I added comments.

--- pe_46.py
+++ pe_46.py
@@ -3,8 +3,7 @@ import math
 import time
 start_time = time.time()
 
-def P(n):
-# This function makes a list of prime numbers smaller than n. I modified an algorithm taken from https://www.geeksforgeeks.org/python-program-to-print-all-prime-numbers-in-an-interval/
+def P(n): # This function makes a list of prime numbers smaller than n. I modified an algorithm taken from https://www.geeksforgeeks.org/python-program-to-print-all-prime-numbers-in-an-interval/
 	pr = [1]
 	for i in range(1, n):
 		if i > 1:
@@ -12,22 +11,20 @@ def P(n):
 				if (i % j == 0):
 					break
 			else:
-				pr.append(i)
-#				print(i)
+				pr.append(i) #When we find a prime number, append it to the list.
 	return pr
 
-max = 10000
+max = 10000 #Make this big enough.
 for k in range(1,max,1):
 	n=2*k+1
-	primes = P(n)
-	if n!= P(n+1)[-1]:
+	primes = P(n+1) #Generate a list of primes smaller than n.
+	if n!= primes[-1]: #This check that n is not prime before getting to work.
 		for i in primes:
-			if (math.sqrt((n-i)/2).is_integer() and math.sqrt((n-i)/2)>10**(-12)):
-				break
-				#Stop the inner loop immediately when there is an x. This saves time.
+			if math.sqrt((n-i)/2).is_integer():
+				break #Stop the inner loop immediately when there is an x. This saves time.
 		if i==primes[-1]:
-			break
+			break #If we reach the end of the list without finding an integer x, then we have solved the problem: Stop.
 if k<max:
 	print(2*k+1)
 
-print("--- %s seconds ---" % (time.time() - start_time)))
+print("--- %s seconds ---" % (time.time() - start_time))
